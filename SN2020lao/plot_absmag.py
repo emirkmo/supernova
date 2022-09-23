4d505e739d1d93574222157e7cf90574e30430a7
Emir K <emir.k@phys.au.dk>
2022-09-23 22:58:01 +0200

    modify 20lao plots to use new interface
    To be honest it doesn't look better, functional may be better. Might have to roll this back

--- SN2020lao/plot_absmag.py
+++ SN2020lao/plot_absmag.py
@@ -1,31 +1,31 @@
+from matplotlib import pyplot as plt
 from supernova import SN
 import supernova.plotting as plot
 import pandas as pd
 import numpy as np
 
+from supernova.plotting.plot_lc import Plotter
+
 # Load SN data.
 lao = SN.from_csv("../SNClass_SN2020lao/")
 laor = lao.restframe()
 
-# Can also set automatic plot shifts & colors, but here using custom ones.
-shifts = [-1, 0, 0.5, 1.5, 1.5]
-laor = plot.make_plot_shifts(sn=laor, reversed_for_abs=True, shifts=shifts)
-laor = plot.make_plot_colors(sn=laor, colors=plot.DEFAULT_COLORS)
 
-# Plot
-fig, ax = plot.make_figure()  # make figure
-# Plot absolute magnitude lcs for each band, with separate markers for each site.
-_, ax = plot.plot_abs_mag(laor, ax=ax, split_by_site=True, label_sites=False, alpha=0.7)
-# Plot limits for 'r' band.
-ax = plot.plot_lims(laor, filt=laor.bands.get('r'), ax=ax, absmag=True, color='r', label='$r$ limit')
-# Label the axes and turn on legend; add some legend parameters for location.
-ax = plot.label_axis(ax, 'Restframe days since explosion', 'Absolute Magnitude [mag]',
-                     legend_kwargs={'loc': (0.59, 0.74), 'ncol': 2})
-# Format the figure, (invert y-axis, set limits, set tick intervals, etc.)
-fig.tight_layout()  # just to make it prettier.
-ax = plot.format_axis(ax, invert=True, xlim=(-10, 98), ylim=ax.get_ylim(),
-                      x_tick_interval=10, y_tick_interval=1)
-# Draw a vertical line at phase zero
+shifts = [-1, 0, 0.5, 1.5, 1.5]
+
+plotpars = plot.PlotParameters(absmag=True, shifts=shifts, split_by_site=True, label_sites=True)
+axpars = plot.AxisParameters(xlabel='Restframe days since explosion', ylabel='Absolute Magnitude',
+                            xlim=(-5, 90), x_tick_interval=10, invert=True,
+                            y_tick_interval=1, legend_kwargs={'loc': (0.59, 0.74), 'ncol': 2})
+plotter = Plotter(laor, plot.plot_abs_mag, plotpars, axpars)
+fig, ax = plotter()
+
+
+plotter.plot_params.band = laor.bands['r']
+ax.set_ylim(ax.get_ylim())
+plotter.update_plot_type(plot.plot_lims, color='red', label='$r$ limit')
+_, ax = plotter.plot(ax)
+
 ax.axvline(0, color='black', linestyle='--', alpha=0.5)
 
 
@@ -38,6 +38,7 @@ ax.vlines(df['restframe'], ymin=ax.get_ylim()[0] * np.ones_like(df.restframe) - 
 
 # Interpolation snake plot
 lao_interp = SN.from_csv("./interp/SNClass_SN2020lao/")
+
 lao_interp = lao_interp.restframe()
 lao_interp = plot.make_plot_shifts(sn=lao_interp, reversed_for_abs=True, shifts=shifts)
 lao_interp = plot.make_plot_colors(sn=lao_interp, colors=plot.DEFAULT_COLORS)
@@ -46,7 +47,55 @@ for name, band in lao_interp.bands.items():
     ax = plot.plot_mag(band_phot, ax=ax, shift=band.plot_shift,
                        error_snake=True, alpha=0.3, color=band.plot_color)
 
-
+#fig.tight_layout()  # just to make it prettier.
 # Display and save
-fig.show()
+#fig.show()
 fig.savefig("absmag2.pdf", dpi=200, format='pdf')
+fig.show()
+plt.show(block=True)
+
+
+# # Can also set automatic plot shifts & colors, but here using custom ones.
+# shifts = [-1, 0, 0.5, 1.5, 1.5]
+# laor = plot.make_plot_shifts(sn=laor, reversed_for_abs=True, shifts=shifts)
+# laor = plot.make_plot_colors(sn=laor, colors=plot.DEFAULT_COLORS)
+
+# # Plot
+# fig, ax = plot.make_figure()  # make figure
+# # Plot absolute magnitude lcs for each band, with separate markers for each site.
+# _, ax = plot.plot_abs_mag(laor, ax=ax, split_by_site=True, label_sites=True, alpha=0.7)
+# # Plot limits for 'r' band.
+# ax = plot.plot_lims(laor, band=laor.bands.get('r'), ax=ax, absmag=True, color='r', label='$r$ limit')
+# # Label the axes and turn on legend; add some legend parameters for location.
+# ax = plot.label_axis(ax, 'Restframe days since explosion', 'Absolute Magnitude [mag]',
+#                      legend_kwargs={'loc': (0.59, 0.74), 'ncol': 2})
+# # Format the figure, (invert y-axis, set limits, set tick intervals, etc.)
+# ax = plot.format_axis(ax, invert=True, xlim=(-10, 98), ylim=ax.get_ylim(),
+#                       x_tick_interval=10, y_tick_interval=1)
+# # Draw a vertical line at phase zero
+# ax.axvline(0, color='black', linestyle='--', alpha=0.5)
+
+
+# # load and plot spectra
+# df = pd.read_json('inputs/spectra_df.json')
+# df['restframe'] = (df['jd']-laor.phase_zero)/(1+laor.redshift)
+# ax.vlines(df['restframe'], ymin=ax.get_ylim()[0] * np.ones_like(df.restframe) - 0.35,
+#           ymax=ax.get_ylim()[0] * np.ones_like(df.restframe),
+#           colors='black', lw=2)
+
+# # Interpolation snake plot
+# lao_interp = SN.from_csv("./interp/SNClass_SN2020lao/")
+# lao_interp = lao_interp.restframe()
+# lao_interp = plot.make_plot_shifts(sn=lao_interp, reversed_for_abs=True, shifts=shifts)
+# lao_interp = plot.make_plot_colors(sn=lao_interp, colors=plot.DEFAULT_COLORS)
+# for name, band in lao_interp.bands.items():
+#     band_phot = lao_interp.band(band.name, return_absmag=True)
+#     ax = plot.plot_mag(band_phot, ax=ax, shift=band.plot_shift,
+#                        error_snake=True, alpha=0.3, color=band.plot_color)
+
+# #fig.tight_layout()  # just to make it prettier.
+# # Display and save
+# #fig.show()
+# fig.savefig("absmag2.pdf", dpi=200, format='pdf')
+# fig.show()
+# plt.show(block=True)
--- SN2020lao/plot_bolo_lum.py
+++ SN2020lao/plot_bolo_lum.py
@@ -1,5 +1,8 @@
+from matplotlib import pyplot as plt
 from supernova import SN
 import supernova.plotting as plot
+from supernova.bolo import bolo_filt, blackbody_filt, ArnettModelFit
+import numpy as np
 
 # load bolometric interpolation data in rest frame
 lao_lum = SN.from_csv("./lum/SNClass_SN2020lao/").restframe()
@@ -7,14 +10,24 @@ lao_lum = SN.from_csv("./lum/SNClass_SN2020lao/").restframe()
 lao_lum.phot.lum /= 1e42
 lao_lum.phot.lum_err /= 1e42
 
+mf = ArnettModelFit.from_json("./lum/SNClass_SN2020lao/arnett.json")
+full = np.linspace(1, 90, 200)
+label = "M$_{ej}$" + f": {mf.with_error('mej')} Mni: {mf.with_error('mni')}" 
+
 # Plot
 fig, ax = plot.make_figure()  # make figure
-ax = plot.plot_band(lao_lum, 'Bolo', ax=ax, plot_type=plot.plot_lum, label='qBol', color='black')
-ax = plot.plot_band(lao_lum, 'BB', ax=ax, plot_type=plot.plot_lum, label='BB', color='purple')
+ax = plot.plot_band(lao_lum, bolo_filt, ax=ax,plot_type=plot.plot_lum,
+                    label='qBol', color='black')
+ax.plot(full, mf.get_model(full)/1e42, color='red', label=label)
+# ax = plot.plot_band(lao_lum, blackbody_filt, ax=ax, plot_type=plot.plot_lum,
+#                     label='BB', color='purple')
 # Label
-bb_phot = lao_lum.band('BB')
+bb_phot = lao_lum.band(bolo_filt.name)
 ax = plot.label_axis(ax, 'Restframe days since explosion', 'Bolometric Luminosity [$10^{42}$ erg/s]')
 ax = plot.format_axis(ax, xlim=(-2, int(bb_phot.phase.max()) + 3),
                       ylim=(0, bb_phot.lum.max() + 0.8), x_tick_interval=10)
-fig.show()
 
+
+fig.show()
+fig.savefig('arnett_fit.pdf', format='pdf')
+plt.show(block=True)
